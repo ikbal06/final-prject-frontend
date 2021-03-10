140ae2e0cb3079a035375f53cded530138a05521
ikbal kırklar <ikbalkirklar@outlook.com>
2021-03-11 01:03:24 +0300

    10.03.21

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/components/models/product';
+import { ProductService } from 'src/app/services/product.service';
+import { ProductResponseModel } from '../models/productResponseModel';
+
 
 @Component({
   selector: 'app-product',
@@ -7,12 +10,20 @@ import { Product } from 'src/app/components/models/product';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
-
-  
   products:Product []= [];
-  constructor() { }
+  dataLoaded=false;
+  
+  constructor(private productService:ProductService) { }
 
   ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts(){
+    this.productService.getProducts().subscribe(response=>{
+     this.products=response.data
+     this.dataLoaded=true;
+    })
   }
 
 }
